f477d82c51e7cc6e78293499a72c7bf83fa3971d
Michaël Cloots <michael.cloots@thomasmore.be>
2021-09-07 09:54:13 +0200

    redux v1

---
+++ src/app/store/actions/user.actions.ts
@@ -0,0 +1,4 @@
+import {createAction} from '@ngrx/store';
+
+export const loginUser = createAction('[Security Component] Login');
+export const logoutUser = createAction('[Security Component] Logout');
